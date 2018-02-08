00491397c4e65111f529541148228422f1d8dc6b
vudoan2016 <vudoant@gmail.com>
2018-02-07 19:24:29 -0800

    Added binary heap implementation

---
+++ heap.c
@@ -0,0 +1,125 @@
+/* 
+ * https://www.cs.cmu.edu/~adamchik/15-121/lectures/Binary%20Heaps/heaps.html
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "heap.h"
+
+static void swap(int *x, int *y)
+{
+  int tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
+
+void heap_init(heap_t *h)
+{
+  h->last = 1; /* first element will be stored at index 1 */
+  h->data = calloc(1, HEAP_MAX*sizeof(int));
+}
+
+void heap_destroy(heap_t *h)
+{
+  free(h->data);
+}
+
+bool heap_insert(heap_t *h, int elem)
+{
+  bool result = false;
+  int parent, cur;
+  
+  if (h->last < HEAP_MAX) {
+    h->data[h->last] = elem;
+    cur = h->last;
+    parent = h->last/2;
+    while (cur > 1 && h->data[cur] < h->data[parent]) {
+      swap(&h->data[cur], &h->data[parent]);
+      cur = parent;
+      parent = cur/2;
+    }
+
+    h->last++;
+    result = true;
+  }
+  return result;
+}
+
+/* 
+ * Swap the last element with the first (index 1) then percolate 
+ * the first element down.
+ */
+int heap_delete(heap_t *h)
+{
+  int cur, left_child, right_child;
+  int min = h->data[1];
+
+  h->data[1] = h->data[--h->last];
+
+  cur = 1;
+  left_child = 2*cur;
+  right_child = 2*cur+1;
+
+  while (left_child < h->last || right_child < h->last) {
+    if (left_child < h->last) {
+      cur = left_child;
+    }
+    if (right_child < h->last && h->data[cur] > h->data[right_child]) {
+      cur = right_child;
+    }
+
+    if (h->data[cur] > h->data[cur/2]) {
+      break;
+    }
+    swap(&h->data[cur], &h->data[cur/2]);
+    left_child = 2*cur;
+    right_child = 2*cur+1;
+  }
+
+  return min;
+}
+
+void heap_print(heap_t *h)
+{
+  int i;
+  
+  printf("Last:%d, ", h->last);
+  for (i = 1; i < h->last; i++) {
+    printf("%d, ", h->data[i]);
+  }
+  printf("\n");
+}
+ 
+ void heap_test()
+ {
+   char *data_file = "heap_data.txt";
+   FILE *fp = fopen(data_file, "r");
+   heap_t *h = NULL;
+   int elem;
+   
+   h = calloc(1, sizeof(heap_t));
+   if (h == NULL) {
+     printf("Unable to allocate memory for a heap\n");
+     return;
+   }
+   
+   heap_init(h);
+   
+   if (fp) {
+     while (!feof(fp)) {
+       if (fscanf(fp, "%d", &elem) == 1) {
+	 heap_insert(h, elem);
+	 heap_print(h);
+       }
+     }
+
+     while (h->last > 1) {
+       elem = heap_delete(h);
+       printf("min %d\n", elem);
+       heap_print(h);
+     }       
+   }
+   heap_destroy(h);
+   free(h);
+   fclose(fp);
+ }
---
+++ heap.h
@@ -0,0 +1,11 @@
+#ifndef __HEAP_H
+#define __HEAP_H
+
+#define HEAP_MAX 100
+
+typedef struct heap_s {
+  int last;
+  int *data;
+} heap_t;
+
+#endif
